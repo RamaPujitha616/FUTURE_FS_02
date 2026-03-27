3cb462bbef59ea7eee78fb11ce2aeacbcd3dcaa1
RamaPujitha616 <ramapujitha.a616@gmail.com>
2026-03-27 23:39:52 +0530

    cleaned

--- crm/server/index.js
+++ crm/server/index.js
@@ -21,7 +21,7 @@ const limiter = rateLimit({
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  origin: process.env.CLIENT_URL ,
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
